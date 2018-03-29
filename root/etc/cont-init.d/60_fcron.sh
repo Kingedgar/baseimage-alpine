1aa28958489e02e5ce6aabdb255d9eca74e03d29
Thomas Fanninger <thomas@fanninger.at>
2018-03-29 21:57:45 +0200

    Bugfix docker file

--- root/etc/cont-init.d/60_fcron.sh
+++ root/etc/cont-init.d/60_fcron.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/with-contenv bash
 
-dockerize -no-overwrite -template /app/crontab.custom:/config/crontab.custom
+dockerize -no-overwrite -template /app/crontab.custom:/conf/crontab.custom
 
 touch /var/spool/fcron/app.orig
-cat /config/crontab.custom > /var/spool/fcron/app.orig
+cat /conf/crontab.custom > /var/spool/fcron/app.orig
