bae73f7c634affc352e996f9bed0b248eb389381
Thomas Fanninger <thomas@fanninger.at>
2018-03-29 18:25:05 +0200

    Bugfix fcron

--- root/etc/cont-init.d/60_fcron.sh
+++ root/etc/cont-init.d/60_fcron.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/with-contenv bash
 
-dockerize -no-overwrite -template /app/crontab.tmp:/config/crontab.tmp
-fcrontab < /config/crontab.tmp
+dockerize -no-overwrite -template /app/crontab.append:/config/crontab.tmp
+
+dockerize -template /conf/crontab:/var/spool/fcron
+cat /config/crontab.append >> /var/spool/fcron
+
+chown root:fcron /var/spool/fcron
+chmod 644 /var/spool/fcron
+
